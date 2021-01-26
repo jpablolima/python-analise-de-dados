38abc8c2307c9df0d798ad50f4f235f65fcff65d
jpablolima <jp.pablolima@gmail.com>
2021-01-26 19:32:27 -0300

    modelo de gráfico ScatterPlot

---
+++ ScatterPlot.py
@@ -0,0 +1,16 @@
+# ScatterPlot
+
+from bokeh.plotting import figure, show, output_file
+from bokeh.sampledata.iris import flowers
+
+colormap = {'setosa': 'red', 'versicolor':'green','virginica':'blue'}
+colors = [colormap[x] for x in flowers['species']]
+
+p = figure(title = 'Iris Morphology')
+p.xaxis.axis_label = 'Petal Length'
+p.yaxis.axis_label = 'Pental Width'
+
+p.circle(flowers['petal_length'], flowers['petal_width'], color=colors, fill_alpha=0.2, size=10)
+output_file('grafico_iris.html', title='iris.py example')
+
+show(p)
