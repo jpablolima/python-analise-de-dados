2db5b61494d3a77539a589741be050cb5d093d32
jpablolima <jp.pablolima@gmail.com>
2021-01-26 19:35:12 -0300

    modelo de gráfico - Dados Geofísicos

---
+++ DadosGeofisicos.py
@@ -0,0 +1,14 @@
+# Dados Geofísicos
+# Modelo de Gráfico 
+
+from bokeh.io import output_file, show
+from bokeh.models import GeoJSONDataSource
+from bokeh.plotting import figure
+from bokeh.sampledata.sample_geojson import geojson
+
+geo_source = GeoJSONDataSource(geojson=geojson)
+
+p = figure()
+p.circle(x = 'x', y = 'y', alpha = 0.9, source =  geo_source)
+output_file('geoJson.html')
+show(p)
